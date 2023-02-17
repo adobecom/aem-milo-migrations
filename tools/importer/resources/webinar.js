e7bfb51769cbd933f8a556ada29efeda5f267051
David Catalan <catalan@adobe.com>
2023-02-17 09:45:02 +0100

    add breadcrumb on all pages

--- tools/importer/resources/webinar.js
+++ tools/importer/resources/webinar.js
@@ -148,49 +148,80 @@ const appendBackward = (elements, main) => {
   }
 }
 
-const createBreadcrumbs = (main, document) => {
-  const breadcrumbsPath = findPaths(window.jcrContent, 'breadcrumbs');
-  if (!breadcrumbsPath?.length) {
-    return '';
-  }
-  let breadcrumbs = window.jcrContent;
-  breadcrumbsPath[0][0]?.split('/').forEach((pathItem) => {
-    breadcrumbs = breadcrumbs[pathItem];
-  });
-  breadcrumbs = breadcrumbs.links;
-  breadcrumbs = Object.values(breadcrumbs);
-  const breadcrumbsLastItem = breadcrumbs.pop();
-  const breadcrumbsItems = document.createElement('ul');
-  const firstItem = document.createElement('li');
-  const firstItemLink = document.createElement('a');
-  firstItemLink.href = '/';
-  firstItemLink.innerHTML = 'Home';
-  firstItem.append(firstItemLink);
-  breadcrumbsItems.append(firstItem);
-  breadcrumbs.forEach((item) => {
-    if (item.title) {
-      let breadcrumbsItem = document.createElement('li');
-      if (item.url) {
-        const breadcrumbLink = document.createElement('a');
-        const url = item.url.split('resources')[1];
-        breadcrumbLink.href = `${window.local ? window.local + '/' : '/'}resources${url}`;
-        breadcrumbLink.innerHTML = item.title;
-        breadcrumbsItem.append(breadcrumbLink);
-      } else {
-        breadcrumbsItem.innerHTML = item.title;
-      }
-      breadcrumbsItems.append(breadcrumbsItem);
+const createBreadcrumbs = (document) => {
+  // default breadcrumb
+  let bcType = 'default';
+  let bcContent = WebImporter.DOMUtils.createTable([['breadcrumbs'],['<ul><li><a href="/">Home</a></li><li>Adobe Resource Center</li></ul>']], document);
+
+  // try building breadcrumb from DOM
+  const domBreadcrumb = document.querySelector('.feds-breadcrumbs');
+  if (domBreadcrumb) {
+    const items = [];
+    const domBreadcrumbItems = domBreadcrumb.querySelectorAll('li');
+    if (domBreadcrumbItems.length > 0) {
+      domBreadcrumbItems.forEach((item) => {
+        const link = item.querySelector('a');
+        if (link) {
+          // make link relative if needed
+          if (link.href.indexOf('http') === 0) {
+            let { pathname } = new URL(link.href);
+            pathname = pathname.replace('.html', '');
+            link.href = pathname;
+          }
+        }
+        items.push(item.outerHTML);
+      });
+  
+      bcType = 'dom';
+      bcContent = WebImporter.DOMUtils.createTable([['breadcrumbs'],[`<ul>${items.join('')}</ul>`]], document);
     }
-  });
-  const lastItem = document.createElement('li');
-  lastItem.innerHTML = breadcrumbsLastItem.title;
-  breadcrumbsItems.append(lastItem);
-  const cells = [
-    ['breadcrumbs'],
-    [breadcrumbsItems],
-  ];
-  const table = WebImporter.DOMUtils.createTable(cells, document);
-  return table;
+  }
+  
+  // try building breadcrumb from jcrContent
+  const breadcrumbsPath = findPaths(window.jcrContent, 'breadcrumbs');
+  if (breadcrumbsPath?.length > 0) {
+    let breadcrumbs = window.jcrContent;
+    breadcrumbsPath[0][0]?.split('/').forEach((pathItem) => {
+      breadcrumbs = breadcrumbs[pathItem];
+    });
+    breadcrumbs = breadcrumbs.links;
+    breadcrumbs = Object.values(breadcrumbs);
+    const breadcrumbsLastItem = breadcrumbs.pop();
+    const breadcrumbsItems = document.createElement('ul');
+    const firstItem = document.createElement('li');
+    const firstItemLink = document.createElement('a');
+    firstItemLink.href = '/';
+    firstItemLink.innerHTML = 'Home';
+    firstItem.append(firstItemLink);
+    breadcrumbsItems.append(firstItem);
+    breadcrumbs.forEach((item) => {
+      if (item.title) {
+        let breadcrumbsItem = document.createElement('li');
+        if (item.url) {
+          const breadcrumbLink = document.createElement('a');
+          const url = item.url.split('resources')[1];
+          breadcrumbLink.href = `${window.local ? window.local + '/' : '/'}resources${url}`;
+          breadcrumbLink.innerHTML = item.title;
+          breadcrumbsItem.append(breadcrumbLink);
+        } else {
+          breadcrumbsItem.innerHTML = item.title;
+        }
+        breadcrumbsItems.append(breadcrumbsItem);
+      }
+    });
+    const lastItem = document.createElement('li');
+    lastItem.innerHTML = breadcrumbsLastItem.title;
+    breadcrumbsItems.append(lastItem);
+    const cells = [
+      ['breadcrumbs'],
+      [breadcrumbsItems],
+    ];
+
+    bcType = 'jcrContent';
+    bcContent = WebImporter.DOMUtils.createTable(cells, document);
+}
+
+  return [ bcType, bcContent ];
 };
 
 
@@ -219,9 +250,11 @@ export default {
 
     cleanupHeadings(document.body);
 
+    const [breadcrumbType, breadcrumb] = createBreadcrumbs(document);
+
     // Top area
     const elementsToGo = [];
-    elementsToGo.push(createBreadcrumbs(main, document));
+    elementsToGo.push(breadcrumb);
     elementsToGo.push(createMarquee(main, document, new URL(params.originalURL)));
     const h2 = document.querySelector('.title h2');
     if (h2) {
