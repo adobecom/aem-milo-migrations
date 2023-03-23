ef5f3b856bbcd2101139d760fbd9830f4a646235
David Catalan <catalan@adobe.com>
2023-03-23 09:21:47 +0100

    fix: not all content was parsed in z-pattern elements

--- tools/importer/rules/z-pattern.js
+++ tools/importer/rules/z-pattern.js
@@ -6,29 +6,34 @@ export async function parseZPattern(el, document, sectionsEls) {
   const cells = [
     ['z-pattern'],
   ];
+  
+  let titleEl = null;
 
   if (sections.length === 1) {
     sections = getNSiblingsElements(sections[0], (n) => n >= 2);
-
+    
     if (!sections[0].querySelector('img')) {
-      const titleEl = sections.shift().querySelector('.title, .text');
-      if (titleEl) {
-        cells.push([titleEl]);
-      }
+      titleEl = sections.shift().querySelector('.title, .text');
     }
   } else {
-    const titleEl = sections[0].querySelector('.title, .text');
-    if (titleEl) {
-      cells.push([titleEl]);
+    titleEl = sections[0].querySelector('.title, .text');
+  }
+  
+  const rowEls = sections.map(section => {
+    const rowTitle = section.querySelector('.title, .text');
+    if (titleEl && rowTitle && rowTitle.textContent === titleEl.textContent) {
+      titleEl = null;
     }
+
+    const rowEls = getNSiblingsElements(section, (c) => c >= 2);
+    return rowEls[0].querySelector('img') ? rowEls : rowEls.reverse();
+  });
+
+  if (titleEl) {
+    cells.push([titleEl]);
   }
 
-  sections.forEach(section => {
-    const textEls = section.querySelectorAll('.text');
-    const textEl = textEls[textEls.length - 1];
-    const imgEl = section.querySelector('.image img');
-    cells.push([imgEl, textEl]);
-  });
+  cells.push(...rowEls);
 
   return WebImporter.DOMUtils.createTable(cells, document);
 }
