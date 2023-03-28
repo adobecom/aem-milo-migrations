113a6625279b133db42123dd103bb82ce4674450
David Catalan <catalan@adobe.com>
2023-03-28 19:24:06 +0200

    fix: background image in aside parser

--- tools/importer/rules/aside.js
+++ tools/importer/rules/aside.js
@@ -6,7 +6,13 @@ export function parseAside(el, document, section) {
   const cells = [['aside (inline, xl spacing)']];
 
   // background color or background image
-  const bgImage = el.querySelector('div[style]')?.getAttribute('style').split('"')[1];
+  let bgImage = el.querySelector('div[style]')?.getAttribute('style').split('"')[1];
+  if (bgImage) {
+    const img = document.createElement('img');
+    img.src = bgImage;
+    bgImage = img;
+  }
+
   let bgcolor = el.querySelector('div[data-bgcolor]')?.getAttribute('data-bgcolor');
 
   // strategy 2
@@ -26,10 +32,13 @@ export function parseAside(el, document, section) {
 
   // content
   const imageContainer = el.querySelector('.image');
-  const image = imageContainer.querySelector('img') || '';
-  const content = imageContainer.nextElementSibling || '';
-  cells.push([image, content]);
-
+  if (imageContainer) {
+    const image = imageContainer.querySelector('img') || '';
+    const content = imageContainer.nextElementSibling || '';
+    cells.push([image, content]);
+  } else {
+    cells.push(['', el.innerHTML]);
+  }
   const table = WebImporter.DOMUtils.createTable(cells, document);
   return table;
   // table.classList.add('import-table');
